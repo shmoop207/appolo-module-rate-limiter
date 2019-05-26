c932e77311213c441b4567b5f4f6f6df657b2387
roman <shmoop207@gmail.com>
2019-05-26 15:38:32 +0300

    first

---
+++ module/src/common/rateLimitType.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var RateLimitType;
+(function (RateLimitType) {
+    RateLimitType["SlidingWindow"] = "SlidingWindow";
+    RateLimitType["FixedWindow"] = "FixedWindow";
+})(RateLimitType = exports.RateLimitType || (exports.RateLimitType = {}));
+//# sourceMappingURL=rateLimitType.js.map
---
+++ module/src/rateLimiterMarshal.js
@@ -0,0 +1,60 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const appolo_1 = require("appolo");
+const rateLimitType_1 = require("./common/rateLimitType");
+let RateLimiterMarshal = class RateLimiterMarshal {
+    prepareParams(roles, type, check) {
+        let dto = [];
+        for (let i = 0, len = roles.length; i < len; i++) {
+            let role = roles[i];
+            let { interval, limit, spread, bucket } = role;
+            bucket = this.windowCalculator.calcBucketInterval({ interval, limit, spread, bucket });
+            spread = this.windowCalculator.calcRateLimit({ interval, limit, spread, bucket });
+            dto.push({
+                window: bucket,
+                interval: role.interval,
+                limit: role.limit,
+                reserve: role.reserve || 1,
+                rateLimit: spread.toString(),
+                check: check,
+                maxWindow: type == rateLimitType_1.RateLimitType.FixedWindow ? (role.start || Date.now() + role.interval) : 0
+            });
+        }
+        return dto;
+    }
+    prepareResults(params, results) {
+        let isValid = true;
+        let dto = [];
+        for (let i = 0, len = results.length; i < len; i++) {
+            let item = results[i], param = params[i];
+            dto.push({
+                count: item[0],
+                bucket: param.window,
+                remaining: param.limit - (item[0]),
+                limit: param.limit,
+                rateLimit: parseFloat(param.rateLimit),
+                rate: parseFloat(item[1]),
+                isValid: !!item[2],
+                reset: item[3],
+                retry: item[4]
+            });
+            if (!item[2]) {
+                isValid = false;
+            }
+        }
+        return {
+            results: dto,
+            isValid
+        };
+    }
+};
+tslib_1.__decorate([
+    appolo_1.inject()
+], RateLimiterMarshal.prototype, "windowCalculator", void 0);
+RateLimiterMarshal = tslib_1.__decorate([
+    appolo_1.define(),
+    appolo_1.singleton()
+], RateLimiterMarshal);
+exports.RateLimiterMarshal = RateLimiterMarshal;
+//# sourceMappingURL=rateLimiterMarshal.js.map
