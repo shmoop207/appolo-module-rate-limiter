59a9acdd2d216d0bfb0141894af16bb3d7e50fa1
roman <shmoop207@gmail.com>
2019-05-26 21:09:47 +0300

    first

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@ import {IOptions} from "./module/src/common/IOptions";
 import {RateLimiterModule} from "./module/rateLimiterModule";
 import {RateLimiter} from "./module/src/rateLimiter";
 import {RateLimitType} from "./module/src/common/rateLimitType"
+import {IRoles,IRole} from "./module/src/common/IRole"
 
-export {RateLimiterModule, RateLimiter, IOptions, RateLimitType}
+export {RateLimiterModule, RateLimiter, IOptions, RateLimitType,IRoles,IRole}
